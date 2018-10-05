7a0c9219a50dd4708dbeff0680c6a6875d105b2b
18031J0083 <43469486+18031J0083@users.noreply.github.com>
2018-10-05 11:49:33 -0700

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- module-3/sortedarray/Solution.java
+++ module-3/sortedarray/Solution.java
@@ -1,14 +1,24 @@
+package module3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-class Sorted
+class Solution
 {
-public static void main(String args[])
+public static void main(String args[]) throws NumberFormatException, IOException
 {
 int k=0,m=0,i;
 Scanner sc=new Scanner(System.in);
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 //System.out.println("size of array-1");
-int n1=sc.nextInt();
+int n1=Integer.parseInt(br.readLine());
 //System.out.println("size of array-2");
-int n2=sc.nextInt();
+int n2=Integer.parseInt(br.readLine());
+String s=br.readLine();
+String s1=br.readLine();
+String s2[]=s.split("[,.]");
+String s3[]=s1.split("[,.]");
 int []a=new int[n1];
 int []b=new int[n2];
 int []c=new int[n1+n2];
@@ -17,12 +27,12 @@ int []c=new int[n1+n2];
        for(i=0;i<n1;i++)
 	    {
 	      //System.out.println("enter the elments of an array1");
-	      a[i]=sc.nextInt();
+	      a[i]=Integer.parseInt(s2[i]);
 	     }
        for(i=0;i<n2;i++)
 	    {
               //System.out.println("enter the elments of an array2");
-	      b[i]=sc.nextInt();
+	      b[i]=Integer.parseInt(s3[i]);
 	    }
 		//System.out.println(n1+n2);
 for(i=0;k<a.length&&m<b.length;i++)
@@ -51,7 +61,10 @@ for(i=0;k<a.length&&m<b.length;i++)
    int g=i;
      for(i=0;i<g;i++)
      {
-	   System.out.print(c[i]);
+    	 if(i<g-1)
+	   System.out.print(c[i]+",");
+    	 else
+    		 System.out.println(c[i]);
      }
 }
 }
